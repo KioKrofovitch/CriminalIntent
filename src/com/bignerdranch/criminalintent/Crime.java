38da517d364c4d8cb14a12a071171ef5b4ece8ef
KioKrofovitch <kio.krofovitch@gmail.com>
2014-02-18 19:09:20 -0700

    End of Chapter 8 Updates to layout, and added a formatted date (extra task).

--- src/com/bignerdranch/criminalintent/Crime.java
+++ src/com/bignerdranch/criminalintent/Crime.java
@@ -1,17 +1,37 @@
 package com.bignerdranch.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
 	
 	private UUID mId;
 	private String mTitle;
+	private Date mDate;  // KIO: used java.util and not java.sql, in case this causes errors later
+	private boolean mSolved;
 	
 	public Crime() {
 		// Generate a unique identifier
 		mId = UUID.randomUUID();
+		mDate = new Date();
 	}
 	
+	public Date getDate() {
+		return mDate;
+	}
+
+	public void setDate(Date date) {
+		mDate = date;
+	}
+
+	public boolean isSolved() {
+		return mSolved;
+	}
+
+	public void setSolved(boolean solved) {
+		mSolved = solved;
+	}
+
 	public UUID getId() {
 		return mId;
 	}
--- src/com/bignerdranch/criminalintent/CrimeFragment.java
+++ src/com/bignerdranch/criminalintent/CrimeFragment.java
@@ -4,15 +4,22 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
 	
 	private Crime mCrime;
 	private EditText mTitleField;
+	private Button mDateButton;
+	private CheckBox mSolvedCheckBox;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -23,8 +30,11 @@ public class CrimeFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent, 
 			Bundle savedInstance) {
+		
+		// Get view for fragment
 		View v = inflater.inflate(R.layout.fragment_crime, parent, false);
 		
+		// Crime Title
 		mTitleField = (EditText) v.findViewById(R.id.crime_title);
 		mTitleField.addTextChangedListener(new TextWatcher() {
 			public void onTextChanged(CharSequence c, int start, int before, int count){
@@ -37,6 +47,21 @@ public class CrimeFragment extends Fragment {
 			
 			public void afterTextChanged(Editable c){
 				// Do something?
+			}
+		});
+		
+		// Date Button
+		mDateButton = (Button)v.findViewById(R.id.crime_date);
+		String formatDate = DateFormat.format("EEEE, MMM dd, yyyy", mCrime.getDate()).toString();
+		mDateButton.setText(formatDate);
+		mDateButton.setEnabled(false); // Disabled only for now; also thats why we didnt set a listener here.
+		
+		// "Solved" Check box
+		mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+		mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				//Set the crime's solved property
+				mCrime.setSolved(isChecked);
 			}
 		});
 		
